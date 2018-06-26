1941f8bb8d04c033b6f2ef6510e07a40e6b3b437
Somtochukwu Nweke <somtonweke1@fb.com>
2018-06-26 13:38:14 -0700

    Initial Commit

---
+++ Tippy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tippy
+//
+//  Created by Somtochukwu Nweke on 6/26/18.
+//  Copyright © 2018 Somtochukwu Nweke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tippy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tippy
+//
+//  Created by Somtochukwu Nweke on 6/26/18.
+//  Copyright © 2018 Somtochukwu Nweke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
